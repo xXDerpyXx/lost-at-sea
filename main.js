49587318bc57ce42054cd7f4e2d39ca51b8bdd5d
ec22583 <ec22583@bristol.ac.uk>
2024-04-07 22:05:31 +0000

    bruh

--- main.js
+++ main.js
@@ -265,7 +265,8 @@ function bodyToString(b,part,layer){
             finalString += "["+bodyToString(b[part],p,layer)+"]"
         }
     }
-    return "```"+finalString+"```"
+
+    return finalString
 }
 
 class player{
